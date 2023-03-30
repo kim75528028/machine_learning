e1c0024e7e32e6fa8def072e6f31f2df1ec78ff2
kim75 <kim75528028@gmail.com>
2023-03-30 17:42:10 +0900

    머신 5주차1

--- week5.py
+++ week5.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
@@ -14,6 +15,9 @@ target = wine['class'].to_numpy()
 
 train_input, test_input, train_target, test_target = train_test_split(
     data, target, test_size=0.2, random_state=42)
+
+sub_input, val_input, sub_target, val_target = train_test_split(
+    train_input, train_target, test_size=0.2, random_state=42)
 
 ss = StandardScaler() #표준화 도구
 ss.fit(train_input)
@@ -43,10 +47,37 @@ from sklearn.tree import plot_tree
 
 dt = DecisionTreeClassifier(max_depth=3, random_state=42)
 dt.fit(train_input, train_target)
-print(dt.score(train_input, train_target))
-print(dt.score(test_input,test_target))
-plt.figure(figsize=(20,15))
-plot_tree(dt, filled=True, feature_names=['alcohol', 'suger', 'pH'])
-plt.show()
+#print(dt.score(train_input, train_target))
+#print(dt.score(test_input,test_target))
+#plt.figure(figsize=(20,15))
+#plot_tree(dt, filled=True, feature_names=['alcohol', 'suger', 'pH'])
+#plt.show()
 
-print(dt.feature_importances_)
+#print(dt.feature_importances_)
+
+from sklearn.model_selection import cross_validate
+
+score = cross_validate(dt, train_input, train_target)
+
+#print(np.mean(score['test_score']))
+
+
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import uniform, randint
+
+params = {'min_impurity_decrease':uniform(0.0001,0.001)
+          'max_depth': randint(20,50)
+          'min_samples_split': randint(2,25)
+          'min_samples_leaf': randint(1,25)}
+gs = RandomizedSearchCV(DecisionTreeClassifier(random_state=42), params, n_iter=100, n_jobs=1, random_state=42)
+gs.fit(train_input, train_target)
+
+print(gs.best_params_)
+print("--------------------------")
+
+print(np.max(gs.cv_results_['mean_test_score']))
+
+print("--------------------------")
+
+dt=gs.best_estimator_
+print(dt.score(test_input, test_target))
