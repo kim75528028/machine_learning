851b3c68b3467dd09f35ba59ba53b999991b4504
kim75 <kim75528028@gmail.com>
2023-03-30 16:40:08 +0900

    머신 5주차

--- week5.py
+++ week5.py
@@ -1,4 +1,28 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 
-wine = pd.read_csv('./wine.csv')
-print(wine.shape)
+#DataFrame ==> np ==> 표준화(평균을 중심으로 동일한 표준편차)
+
+wine = pd.read_csv('wine.csv')
+data = wine[['alcohol', 'sugar', 'pH']].to_numpy()
+target = wine['class'].to_numpy()
+
+train_input, test_input, train_target, test_target = train_test_split(
+    data, target, test_size=0.2, random_state=42)
+
+ss = StandardScaler() #표준화 도구
+ss.fit(train_input)
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+
+lr = LogisticRegression()
+lr.fit(train_scaled, train_target)
+
+print(lr.score(train_scaled, train_target))
+print(lr.score(test_scaled, test_target))
+print(lr.coef_, lr.intercept_)
