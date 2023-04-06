ba47828892e0534308a02f8915703bf9198b6ca7
kim75 <kim75528028@gmail.com>
2023-04-06 17:35:15 +0900

    머신 러닝 6주차

--- week6.py
+++ week6.py
@@ -59,12 +59,14 @@ def draw_fruits(arr, ratio=1):
     n = len(arr)
     rows = int(np.ceil(n/10))
     cols = n if rows < 2 else 10
-    fig, axs = plt.subplots(rows, cols, figsize=(col*ratio, row*ratio),squeeze=False)
+    fig, axs = plt.subplots(rows, cols,
+                            figsize=(cols*ratio, rows*ratio), squeeze=False)
     for i in range(rows):
         for j in range(cols):
             if i*10 + j < n:
-                axs[i,j].imshow(arr[i*10 + j], camp='gray_r')
-                axs[i,j].axis(off)
+                axs[i, j].imshow(arr[i*10 + j], cmap='gray_r')
+            axs[i, j].axis('off')
+
 
 fruits_2d = fruits.reshape(-1, 100*100)
 from sklearn.decomposition import PCA
